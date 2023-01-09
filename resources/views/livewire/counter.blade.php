ddd0cdbb22efd4a4c363830d6b0850594e35da53
liakatbiswas <87108284+liakatbiswas@users.noreply.github.com>
2023-01-09 12:52:48 +0600

    class 3 Livewire and PHP Flasher Done!

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <p>{{ $count }}</p>
+
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error esse voluptate quam distinctio pariatur.
+        Molestiae odit perspiciatis incidunt, non accusamus recusandae numquam minus voluptatem necessitatibus dolores,
+        quasi veniam. Rerum, deleniti!</p>
+
+
+    <button
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        wire:click="increase">Increase</button>
+
+</div>
